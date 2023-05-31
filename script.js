f866103da9062b351197833a5e6a1760d2a451a6
S.Zelensky <zelenstein@gmail.com>
2023-05-31 21:40:38 +0300

    translate

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let x1 = 0;
-let y1 = 0;
+const sphere1 = document.getElementById('sphere1');
+
+let x1 = 1000;
+let y1 = 1000;
 let vx1 = 0;
 let vy1 = 0;
 let ax1 = 0;
 let ay1 = 0;
 
-const sphere1 = document.getElementById('sphere1');
-
 if (window.DeviceMotionEvent != undefined) {
   window.ondevicemotion = function (e) {
-    ax1 = event.accelerationIncludingGravity.x * 5;
-    ay1 = event.accelerationIncludingGravity.y * 5;
+    ax1 = e.accelerationIncludingGravity.x * 5;
+    ay1 = e.accelerationIncludingGravity.y * 5;
   };
   setInterval(function () {
     const landscapeOrientation = window.innerWidth / window.innerHeight > 1;
@@ -24,13 +24,14 @@ if (window.DeviceMotionEvent != undefined) {
     vx1 = vx1 * 0.98;
     vy1 = vy1 * 0.98;
     y1 = parseInt(y1 + vy1 / 50);
+    console.log('y1:', y1)
     x1 = parseInt(x1 + vx1 / 50);
+    console.log('x1:', x1)
 
     boundingBoxCheck();
 
-    sphere1.style.bottom = y1 + 'px';
-    sphere1.style.right = x1 + 'px';
-  }, 25);
+    sphere1.style.transform = `translate(' + ${y1} + 'px', ${x1} + 'px')`;
+  }, 1000);
 }
 function boundingBoxCheck() {
   if (x1 < 0) {
